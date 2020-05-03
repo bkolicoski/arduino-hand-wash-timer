236be2ac20f033c784d9cefd234d0d5d88b5345c
Bill Kolicoski <bkolicoski@gmail.com>
2020-05-03 12:58:08 +0200

    Added initial code version

---
+++ HandwashTimer/HandwashTimer.ino
@@ -0,0 +1,82 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 6
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(12, PIN, NEO_GRB + NEO_KHZ800);
+
+const int analogInPin = A3;
+
+int sensorValue = 0;
+
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+  strip.begin();
+  strip.setBrightness(50);
+  strip.show();
+
+  delay(5000);
+  colorWipe(strip.Color(255, 0, 0), 50);
+  strip.clear();
+  strip.show();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensorValue = analogRead(analogInPin);
+  Serial.println(sensorValue);
+  if (sensorValue < 100) {
+    rainbowCycle(15);
+    strip.clear();
+    strip.show();
+  }
+  delay(100);
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# arduino-hand-wash-timer
+# Arduino hand wash timer 
 Arduino based hand wash timer with RGB NeoPixel ring
+
+This code is used in my project video at: [https://youtu.be/rFSXo7EFj_Q](https://youtu.be/rFSXo7EFj_Q)
